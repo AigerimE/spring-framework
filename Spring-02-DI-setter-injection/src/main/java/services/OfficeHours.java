a45521977ac08707934eb0fd0c9d4e67160a6fc5
aigerimnurlan <aechka92@gmail.com>
2020-10-20 19:07:46 -0500

    dependency injection

---
+++ Spring-02-DI-setter-injection/src/main/java/services/OfficeHours.java
@@ -0,0 +1,9 @@
+package services;
+
+public class OfficeHours {
+
+
+    public int getHours() {
+        return 5;
+    }
+}
---
+++ Sprint-02-DI-constructorInjection/src/main/java/interfaces/Course.java
@@ -0,0 +1,4 @@
+package com.interfaces.services;
+
+public interface Course {
+}
