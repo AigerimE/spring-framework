09a08dda05239f8270ebe0581e52352dabb79c21
aigerimnurlan <aechka92@gmail.com>
2020-10-24 10:02:27 -0500

    Added component

---
+++ Spring-03-BeanScopeSingleton-prototype/src/main/java/services/Java.java
@@ -0,0 +1,19 @@
+package services;
+
+import interfaces.Course;
+import interfaces.ExtraSessions;
+
+public class Java implements Course {
+
+    OfficeHours officeHours;
+    ExtraSessions extraSessions;
+
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : " + (20 + officeHours.getHours()));
+    }
+}
---
+++ Spring-04-BeanLyfeCycle/src/main/java/com/Main.java
@@ -0,0 +1,16 @@
+import interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course1 = context.getBean("java", Course.class);
+
+        Course course2 = context.getBean("java", Course.class);
+
+        System.out.println("pointing to othe same object :" + (course1));
+    }
+}
